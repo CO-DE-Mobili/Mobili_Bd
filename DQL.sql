5ec9ba8645fd06d582598824d242f06d296d34b0
biribrinha <juliabergman523@gmail.com>
2023-11-22 21:54:24 -0300

    feat: mais selects no dql

--- DQL.sql
+++ DQL.sql
@@ -1,9 +1,29 @@
 -- DQL
 
 -- SELECT TABELA INTEIRA
+SELECT * FROM tb_parceiro;
+
+SELECT * FROM tb_empresa;
+
+SELECT * FROM tb_categoria;
+
+SELECT * FROM tb_dias_funcionamento;
+
+SELECT * FROM tb_redes_sociais;
+
+SELECT * FROM tb_servico;
+
+SELECT * FROM tb_motorista;
+
 SELECT * FROM tb_motorista_preferencias;
+
+SELECT * FROM tb_servico_categoria;
+
+
 -- SELECT UUID
 
 SELECT BIN_TO_UUID(id), nome FROM tb_servico;
+
 SELECT BIN_TO_UUID(id), nome FROM tb_motorista;
+
 SELECT BIN_TO_UUID(id), nome FROM tb_categoria;
